0aaef55cfe792f9837d8e26ff358175fc906f048
Kkenize <Kkenizez@gmail.com>
2025-01-19 18:18:53 -0500

    This is my first practice for week one hoomework.

--- HomeworkPracticeWeekOne/ContentView.swift
+++ HomeworkPracticeWeekOne/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "My First Swift App!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.yellow)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.yellow)
+            HStack {
+                Button("Let's Go!") {
+                    message = "My First Swift App!"
+                }
+                .buttonStyle(.borderedProminent)
+                .bold()
+                .font(.title2)
+                .tint(.blue)
+                Button("Let's Go!") {
+                    message = "My First Swift App!"
+                }
+                .buttonStyle(.borderedProminent)
+                .bold()
+                .font(.title2)
+                .tint(.red)
+            }
         }
         .padding()
     }
